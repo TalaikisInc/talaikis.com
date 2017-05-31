f260896b2caf8897b010e579ec5186e253b6da5d
Tadas Talaikis <talaikis.tadas@gmail.com>
2017-05-31 11:17:03 +0300

    added uwsgi installer

--- uwsgi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=/usr/local/anaconda/lib:$LD_LIBRARY_PATH
-
-source /usr/local/anaconda/bin/activate /usr/local/anaconda/envs/talaikis && /usr/local/anaconda/envs/talaikis/bin/uwsgi --ini /home/talaikis/emperor.ini
-
-source /usr/local/anaconda/bin/deactivate
